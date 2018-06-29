e9eaad453ad942f711eb263d099ea834938b743d
Pawel Bus <pawelbus.dev@outlook.com>
2018-06-29 17:13:24 +0200

    Add client request support

--- modules/server.js
+++ modules/server.js
@@ -1,12 +1,26 @@
 var http = require('http'),
     colors = require('colors');
 
+var handlers = require('./handlers');
+
 function start() {
   function onRequest(request, response) {
     console.log('Odebrano zapytanie.');
+    console.log('Zapytanie ' + request.url + ' odebrane.');
+
     response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    response.write('Pierwsze koty za płoty');
-    response.end();
+    
+    switch (request.url) { // switch rozróżniający zapytania
+        case '/':
+        case '/start':
+            handlers.welcome(request, response);
+            break;
+        case '/upload':
+            handlers.upload(request, response);
+            break;
+        default:
+            handlers.error(request, response);
+    }
   }
 
   http.createServer(onRequest).listen(9000);
